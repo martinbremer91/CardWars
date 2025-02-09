8a8ef21c96bce96d09ccac461bb7881059e9a062
Martin Bremer <martin.bremer91@gmail.com>
2025-02-09 18:52:52 +0100

    reset networking scripts to original references

--- resources/client.py
+++ resources/client.py
@@ -1,33 +1,61 @@
 import socket
+import sys
 import threading
-import urllib.request
 
-self_ip = urllib.request.urlopen('https://v4.ident.me/').read().decode('utf8')
-peer_ip = input('peer ip: ')
+rendezvous = ('147.182.184.215', 55555)
 
-socket_send = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-socket_send.bind(('0.0.0.0', 0))
-source_port = socket_send.getsockname()[1]
-print(source_port)
+# connect to rendezvous
+print('connecting to rendezvous server')
 
-destination_port = int(input('peer port: '))
+sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+sock.bind(('0.0.0.0', 50001))
+sock.sendto(b'0', rendezvous)
 
-socket_send.sendto(b'0', (peer_ip, destination_port))
+while True:
+    data = sock.recv(1024).decode()
 
+    if data.strip() == 'ready':
+        print('checked in with server, waiting')
+        break
+
+data = sock.recv(1024).decode()
+ip, sport, dport = data.split(' ')
+sport = int(sport)
+dport = int(dport)
+
+print('\ngot peer')
+print('  ip:          {}'.format(ip))
+print('  source port: {}'.format(sport))
+print('  dest port:   {}\n'.format(dport))
+
+# punch hole
+# equiv: echo 'punch hole' | nc -u -p 50001 x.x.x.x 50002
+print('punching hole')
+
+sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+sock.bind(('0.0.0.0', sport))
+sock.sendto(b'0', (ip, dport))
+
+print('ready to exchange messages\n')
+
+# listen for
+# equiv: nc -u -l 50001
 def listen():
-    socket_listen = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    socket_listen.bind(('0.0.0.0', source_port))
+    sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    sock.bind(('0.0.0.0', sport))
 
     while True:
-        data = socket_listen.recv(1024)
+        data = sock.recv(1024)
         print('\rpeer: {}\n> '.format(data.decode()), end='')
 
-listener = threading.Thread(target=listen, daemon=True)
+listener = threading.Thread(target=listen, daemon=True);
 listener.start()
 
-socket_send = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-socket_send.bind(('0.0.0.0', destination_port))
+# send messages
+# equiv: echo 'xxx' | nc -u -p 50002 x.x.x.x 50001
+sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+sock.bind(('0.0.0.0', dport))
 
 while True:
     msg = input('> ')
-    socket_send.sendto(msg.encode(), (peer_ip, source_port))
+    sock.sendto(msg.encode(), (ip, sport))
