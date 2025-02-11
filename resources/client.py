22e42e1ab0adb6673162ae0c0d390d3fcdf72d53
Martin Bremer <martin.bremer91@gmail.com>
2025-02-11 22:44:06 +0100

    new test for network logic

--- resources/client.py
+++ resources/client.py
@@ -1,61 +1,23 @@
 import socket
-import sys
-import threading
 
-rendezvous = ('147.182.184.215', 55555)
+HEADER = 64
+PORT = 5050
+FORMAT = 'utf-8'
+DISCONNECT_MESSAGE = "!DISCONNECT"
+SERVER = socket.gethostbyname(socket.gethostname())
+ADDR = (SERVER, PORT)
 
-# connect to rendezvous
-print('connecting to rendezvous server')
+client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+client.connect(ADDR)
 
-sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-sock.bind(('0.0.0.0', 50001))
-sock.sendto(b'0', rendezvous)
+def send(msg):
+    message = msg.encode(FORMAT)
+    msg_length = len(message)
+    send_length = str(msg_length).encode(FORMAT)
+    send_length += b' ' * (HEADER - len(send_length))
+    client.send(send_length)
+    client.send(message)
+    print(client.recv(2048).decode(FORMAT))
 
-while True:
-    data = sock.recv(1024).decode()
-
-    if data.strip() == 'ready':
-        print('checked in with server, waiting')
-        break
-
-data = sock.recv(1024).decode()
-ip, sport, dport = data.split(' ')
-sport = int(sport)
-dport = int(dport)
-
-print('\ngot peer')
-print('  ip:          {}'.format(ip))
-print('  source port: {}'.format(sport))
-print('  dest port:   {}\n'.format(dport))
-
-# punch hole
-# equiv: echo 'punch hole' | nc -u -p 50001 x.x.x.x 50002
-print('punching hole')
-
-sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-sock.bind(('0.0.0.0', sport))
-sock.sendto(b'0', (ip, dport))
-
-print('ready to exchange messages\n')
-
-# listen for
-# equiv: nc -u -l 50001
-def listen():
-    sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    sock.bind(('0.0.0.0', sport))
-
-    while True:
-        data = sock.recv(1024)
-        print('\rpeer: {}\n> '.format(data.decode()), end='')
-
-listener = threading.Thread(target=listen, daemon=True);
-listener.start()
-
-# send messages
-# equiv: echo 'xxx' | nc -u -p 50002 x.x.x.x 50001
-sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-sock.bind(('0.0.0.0', dport))
-
-while True:
-    msg = input('> ')
-    sock.sendto(msg.encode(), (ip, sport))
+send("Hello World!")
+send(DISCONNECT_MESSAGE)
--- resources/server.py
+++ resources/server.py
@@ -1,29 +1,43 @@
 import socket
+import threading
 
-known_port = 50002
+HEADER = 64
+PORT = 5050
+SERVER = socket.gethostbyname(socket.gethostname())
+ADDR = (SERVER, PORT)
+FORMAT = 'utf-8'
+DISCONNECT_MESSAGE = "!DISCONNECT"
 
-sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-sock.bind(('0.0.0.0', 55555))
+server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+server.bind(ADDR)
 
-while True:
-    clients = []
+def handle_client(conn, addr):
+    print(f"[NEW CONNECTION] {addr} connected.")
 
+    connected = True
+    while connected:
+        msg_length = conn.recv(HEADER).decode(FORMAT)
+        if msg_length:
+            msg_length = int(msg_length)
+            msg = conn.recv(msg_length).decode(FORMAT)
+
+            if msg == DISCONNECT_MESSAGE:
+                connected = False
+
+            print(f"[{addr}] {msg}")
+            conn.send("Message received".encode(FORMAT))
+
+    conn.close()
+
+def start():
+    server.listen()
+    print(f"[LISTENING] Server is listening on {SERVER}")
     while True:
-        data, address = sock.recvfrom(128)
+        conn, addr = server.accept()
+        thread = threading.Thread(target=handle_client, args=(conn, addr))
+        thread.daemon = True
+        thread.start()
+        print(f"[ACTIVE CONNECTIONS] {threading.active_count() - 1}")
 
-        print('connection from: {}'.format(address))
-        clients.append(address)
-
-        sock.sendto(b'ready', address)
-
-        if len(clients) == 2:
-            print('got 2 clients, sending details to each')
-            break
-
-    c1 = clients.pop()
-    c1_addr, c1_port = c1
-    c2 = clients.pop()
-    c2_addr, c2_port = c2
-
-    sock.sendto('{} {} {}'.format(c1_addr, c1_port, known_port).encode(), c2)
-    sock.sendto('{} {} {}'.format(c2_addr, c2_port, known_port).encode(), c1)
+print("[LISTENING] server is starting...")
+start()
